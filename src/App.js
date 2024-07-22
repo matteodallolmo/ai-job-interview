9af325cb48d6fe8c1cf0494b0c46feb128f992d0
rajthaker13 <69283649+rajthaker13@users.noreply.github.com>
2024-07-22 07:29:18 -0700

    early

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createClient } from "@supabase/supabase-js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
 
 function App() {
+  const supabaseUrl = "https://twqwiryumiolecfebqtd.supabase.co";
+  const supabaseKey =
+    "REDACTED_SECRET";
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login db={supabase} isNewAccount={true} />} />
+        <Route
+          path="/login"
+          element={<Login db={supabase} isNewAccount={false} />}
+        />
+        <Route path="/home" element={<Home db={supabase} />} />
+      </Routes>
+    </Router>
   );
 }
 
