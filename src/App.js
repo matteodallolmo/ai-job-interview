a005a669d7e492ac6a4f0e1484cffbb9fffd582d
NingyueLiang <71284395+NingyueLiang@users.noreply.github.com>
2024-07-26 10:55:54 -0500

    Update with Supabase Auth UI

--- src/App.js
+++ src/App.js
@@ -3,25 +3,66 @@ import { createClient } from "@supabase/supabase-js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./screens/Login";
 import Home from "./screens/Home";
+import { useState, useEffect } from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
 
-function App() {
-  const supabaseUrl = "https://twqwiryumiolecfebqtd.supabase.co";
-  const supabaseKey =
-    "REDACTED_SECRET";
-  const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = "https://twqwiryumiolecfebqtd.supabase.co";
+const supabaseKey =
+  "REDACTED_SECRET";
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login db={supabase} isNewAccount={true} />} />
-        <Route
-          path="/login"
-          element={<Login db={supabase} isNewAccount={false} />}
-        />
-        <Route path="/home" element={<Home db={supabase} />} />
-      </Routes>
-    </Router>
-  );
+export default function App() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return (
+      <div className="flex justify-center align-middle text-center h-[100vh]">
+        <div className="bg-white h-[auto] py-80 rounded-lg w-[40vw]">
+          <Auth
+            supabaseClient={supabase}
+            appearance={{ theme: ThemeSupa }}
+            providers={["google", "github"]}
+          />
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Logged in!</div>;
+  }
 }
+// function App() {
+//   const supabaseUrl = "https://twqwiryumiolecfebqtd.supabase.co";
+//   const supabaseKey =
+//     "REDACTED_SECRET";
+//   const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default App;
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Login db={supabase} isNewAccount={true} />} />
+//         <Route
+//           path="/login"
+//           element={<Login db={supabase} isNewAccount={false} />}
+//         />
+//         <Route path="/home" element={<Home db={supabase} />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
